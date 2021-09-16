ad6e3a26caa5c641271cc70da09d0563a16e0c5a
anutchai <anuschai.bsod@gmail.com>
2021-09-16 16:42:35 +0700

    เขียน Form

--- lib/main.dart
+++ lib/main.dart
@@ -2,8 +2,6 @@ import 'package:flutter/material.dart';
 import 'package:mobileappweek1/config/constant.dart';
 import 'package:mobileappweek1/screen/login.dart';
 import 'package:mobileappweek1/screen/register.dart';
-
-import 'screen/home.dart';
 import 'screen/index.dart';
 
 void main() => runApp(MyApp());
