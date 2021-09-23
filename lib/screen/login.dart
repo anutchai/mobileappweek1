2d781ad32367809358a8cdefabb4e33a7cdf3d21
anutchai <anuschai.bsod@gmail.com>
2021-09-23 15:32:56 +0700

    Update Login Form

--- lib/screen/login.dart
+++ lib/screen/login.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:mobileappweek1/config/constant.dart';
 
 class Login extends StatefulWidget {
   @override
@@ -7,12 +8,89 @@ class Login extends StatefulWidget {
 
 class _LoginState extends State<Login> {
   @override
+  @override
   Widget build(BuildContext context) {
+    Size size = MediaQuery.of(context).size;
     return Scaffold(
-      appBar: AppBar(
-        title: Text('Login'),
+      body: Container(
+        alignment: Alignment.center,
+        child: SingleChildScrollView(
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: <Widget>[
+              SizedBox(
+                height: size.height * 0.03,
+              ),
+              Text(
+                'KMUTNB Login',
+                style: TextStyle(fontSize: 25, color: pColor),
+              ),
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              Container(
+                width: size.width * 0.9,
+                child: TextFormField(
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                  decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.alternate_email,
+                      color: pColor,
+                      size: size.height * 0.05,
+                    ),
+                    hintText: "Email",
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.04,
+              ),
+              Container(
+                width: size.width * 0.9,
+                child: TextFormField(
+                  style: TextStyle(color: Colors.black, fontSize: 20),
+                  obscureText: true,
+                  decoration: InputDecoration(
+                    icon: Icon(
+                      Icons.lock,
+                      color: pColor,
+                      size: size.height * 0.05,
+                    ),
+                    hintText: "Password",
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.05,
+              ),
+              Container(
+                width: size.width * 0.8,
+                height: size.height * 0.08,
+                child: RaisedButton(
+                  shape: RoundedRectangleBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                      side: BorderSide(color: pColor)),
+                  onPressed: () {},
+                  color: pColor,
+                  textColor: Colors.white,
+                  child: Text("login".toUpperCase(),
+                      style: TextStyle(fontSize: 16)),
+                ),
+              ),
+              SizedBox(
+                height: size.height * 0.05,
+              ),
+              Text(
+                'All rights reserved',
+                style: TextStyle(
+                  fontSize: 16,
+                  color: sColor,
+                ),
+              ),
+            ],
+          ),
+        ),
       ),
-      body: Text('Hello World'),
     );
   }
 }
